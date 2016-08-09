70bd2d8d64ee6321a7e38c314bf159694b3a52aa
Michael Williams <mw@mikewill.net>
2016-08-09 19:56:04 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(starting_int)
+  count = 0
+
+  while count < starting_int
+    number = starting_int - count
+    puts "#{number} SECOND(S)!"
+    count += 1
+  end
+
+  return "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(starting_int)
+  iter = 0
+
+  while iter < starting_int
+   countdown(starting_int)
+   sleep(1)
+   iter += 1
+  end
+  
 end
